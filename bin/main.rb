ff1347f184bfa0dcdebb1a0caee87e555617f6d4
franklin <franklinbenjamin032@gmail.com>
2021-01-21 13:43:49 -0400

    update main.rb, second milestone

--- bin/main.rb
+++ bin/main.rb
@@ -1,23 +1,27 @@
 #!/usr/bin/env ruby
+# rubocop:disable Metrics/CyclomaticComplexity
+# rubocop:disable Metrics/PerceivedComplexity
 puts 'Welcome to Tic-Tac-Toe'
 
 class Player
-  def initialize(name, token)
+  attr_accessor :name, :token
+  def initialize(name, token = 'x')
     @name = name
     @token = token
   end
-
+end
 
 puts 'Player 1: please enter your name'
-
-player1 = gets.chomp
+name1 = gets.chomp
+player1 = Player.new(name1)
 
 puts 'Player 2: please enter your name'
+name2 = gets.chomp
+player2 = Player.new(name2, 'o')
 
-player2 = gets.chomp
 puts '--- each one of you will select one place at a time, wins the first one to place 3 tokens in a row---'
 
-board = ''
+board = Array.new(9, '')
 
 def display_board(board)
   puts ''
@@ -31,11 +35,10 @@ end
 
 display_board(board)
 
-puts "#{player1} your token will be x"
-puts "#{player2} your token will be o"
+puts "#{player1} your token will be #{player1.token}"
+puts "#{player2} your token will be #{player2.token}"
 
 puts '------------------------------------'
-
 
 # here we call de position_change method to insert the playes's input into the board
 display_board(board)
@@ -65,12 +68,40 @@ puts " congratulations #{player1}!!! you've won the game"
 puts 'wheww! we\'ve got a tie here! let\'s start all over again!'
 # here we would call an init function to restart the game
 
-def game_play
+def game_play(board)
   limit = true
-  counter = 0
   while limit
-    puts "#{player} please select your move:"
-    
-  
-
+    player = turn(board, 1) # method to be defined
+    puts "#{player} please select your move: between 0-8, on unoccupied spaces"
+    puts board
+    position = gets.chomp
+    move_index = position.to_i
+    if game_tied? || won? || board_full? # methods to be defined
+      limit = false
+    elsif valid_move?(move_index)
+      token = player.token
+      board[move_index] = token
+      change_turns(player) # method to be defined
+    elsif !valid_move(move_index)
+      puts 'please try again!'
+    end
+  end
 end
+
+def game_full?(board)
+  board.all? { |current| current == 'x' || 'o' }
+end
+
+def game_tied?
+  !won? && board_full?
+end
+
+def valid_move?(move_index)
+  if (board[move_index] == ('x' || 'o')) || (move_index > board.length)
+    false
+  else
+    true
+  end
+end
+# rubocop:enable Metrics/CyclomaticComplexity
+# rubocop:enable Metrics/PerceivedComplexity
