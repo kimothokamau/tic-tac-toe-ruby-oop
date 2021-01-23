c2e2d5e5bcd569c6aefb9d05d644ce11f02ba147
kimothokamau <kimothokamau@gmail.com>
2021-01-23 03:20:24 -0600

    Added game logic

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,37 @@
 #!/usr/bin/env ruby
-puts 'Hello!'
+require_relative '../lib/logic.rb'
+
+def intro
+  puts 'Welcome to the Tic-Tac-Toe Game!'
+  display_board
+end
+
+def turn
+  player = current_player
+  puts "Player #{player}, please enter a number between 0 and 8"
+  user_input = gets.chomp
+  index = input_to_index(user_input)
+  if valid_move?(index)
+    move(index, player)
+    @board[index] = player
+    display_board
+    puts "Hey player #{player}, you selected #{index} and its turn #{turn_count}"
+  else
+    puts 'Please try again and enter a valid number'
+  end
+end
+
+public
+
+def play
+  intro
+  turn until over?
+  if won?
+    puts "Congratulations player #{winner}, you won!"
+  else
+    puts 'Its a draw, play again!'
+  end
+end
+
+tic_tac_toe = TicTacToe.new
+tic_tac_toe.play
