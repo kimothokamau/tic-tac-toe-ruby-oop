2abfb56d868e9611abbb4e8193d2ecdc970741e4
kimothokamau <kimothokamau@gmail.com>
2021-01-22 23:18:28 -0600

    Added game play and user interface

--- bin/main.rb
+++ bin/main.rb
@@ -1,107 +1,123 @@
-#!/usr/bin/env ruby
-# rubocop:disable Metrics/CyclomaticComplexity
-# rubocop:disable Metrics/PerceivedComplexity
-puts 'Welcome to Tic-Tac-Toe'
+# frozen_string_literal: true
 
-class Player
-  attr_accessor :name, :token
-  def initialize(name, token = 'x')
-    @name = name
-    @token = token
+class TicTacToe
+  def initialize
+    @board = Array.new(9, ' ')
+    @turn_count = turn_count
   end
-end
 
-puts 'Player 1: please enter your name'
-name1 = gets.chomp
-player1 = Player.new(name1)
+  WIN_COMBINATIONS = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
+  ].freeze
 
-puts 'Player 2: please enter your name'
-name2 = gets.chomp
-player2 = Player.new(name2, 'o')
+  def display_board
+    puts ' '
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts ' ---------- '
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts ' ---------- '
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    puts ' '
+  end
 
-puts '--- each one of you will select one place at a time, wins the first one to place 3 tokens in a row---'
+  def intro
+    puts 'Welcome to the Tic-Tac-Toe Game!'
+    display_board
+  end
 
-board = Array.new(9, '')
+  def input_to_index(user_input)
+    user_input.to_i
+  end
 
-def display_board(board)
-  puts ''
-  puts " #{board[0]}  | #{board[1]}  | #{board[2]} "
-  puts '--- --- ---'
-  puts " #{board[3]}  | #{board[4]}  | #{board[5]} "
-  puts '--- --- ---'
-  puts " #{board[6]}  | #{board[7]}  | #{board[8]} "
-  puts ''
-end
+  def move(input_to_index, current_player)
+    @board[input_to_index] = current_player
+  end
 
-display_board(board)
+  def position_taken?(index)
+    @board[index] == 'X' || @board[index] == 'O'
+  end
 
-puts "#{player1} your token will be #{player1.token}"
-puts "#{player2} your token will be #{player2.token}"
+  def valid_move?(index)
+    !position_taken?(index) && (0..8).include?(index)
+  end
 
-puts '------------------------------------'
+  def turn_count
+    count = 0
+    @board.each do |i|
+      count += 1 if %w[X O].include?(i)
+    end
+    count
+  end
 
-# here we call de position_change method to insert the playes's input into the board
-display_board(board)
-puts 'this is the current state of the board, next player'
+  def current_player
+    if turn_count.even?
+      'X'
+    else
+      'O'
+    end
+  end
 
-puts "#{player2} please select your move"
-# here we call de position_change method to insert the playes's input into the board
-display_board(board)
-puts 'this is the current state of the board, next player'
+  def turn
+    player = current_player
+    puts "Player #{player}, please enter a number between 0 and 8"
+    user_input = gets.chomp
+    index = input_to_index(user_input)
+    if valid_move?(index)
+      move(index, player)
+      @board[index] = player
+      display_board
+      puts "Hey player #{player}, you selected #{index} and its turn #{turn_count}"
+    else
+      puts 'Please try again and enter a valid number'
+    end
+  end
 
-#---in here we are going to display just the exceptional messages of the game, without the logic part or them organized
+  def won?
+    WIN_COMBINATIONS.select do |combination|
+      choice1 = @board[combination[0]]
+      choice2 = @board[combination[1]]
+      choice3 = @board[combination[2]]
+      if choice1 == 'X' && choice2 == 'X' && choice3 == 'X'
+        return combination
+      elsif choice1 == 'O' && choice2 == 'O' && choice3 == 'O'
+        return combination
+      end
+    end
+    false
+  end
 
-# in case the player in turn chooses an already occupied space in the board
-puts 'invalid input: position already occupied, try again'
-display_board(board)
-puts 'this is the current state of the board, next player'
+  def full?
+    @board.all? { |i| i != ' ' && !i.nil? }
+  end
 
-# in case the player chooses a space minor or bigger than the spaces available
-puts 'invalid input: position out of bounds, try again'
-display_board(board)
-puts 'this is the current state of the board, next player'
+  def draw?
+    full? && !won?
+  end
 
-# in case one of the players accomplishes a winning pattern
-puts " congratulations #{player1}!!! you've won the game"
+  def over?
+    draw? || won?
+  end
 
-# in case the board is full and none of the players win
-puts 'wheww! we\'ve got a tie here! let\'s start all over again!'
-# here we would call an init function to restart the game
+  def winner
+    combination = won?
+    if combination
+      if combination.all? { |i| @board[i] == 'X' }
+        'X'
+      elsif combination.all? { |i| @board[i] == 'O' }
+        'O'
+      end
+    end
+  end
 
-def game_play(board)
-  limit = true
-  while limit
-    player = turn(board, 1) # method to be defined
-    puts "#{player} please select your move: between 0-8, on unoccupied spaces"
-    puts board
-    position = gets.chomp
-    move_index = position.to_i
-    if game_tied? || won? || board_full? # methods to be defined
-      limit = false
-    elsif valid_move?(move_index)
-      token = player.token
-      board[move_index] = token
-      change_turns(player) # method to be defined
-    elsif !valid_move(move_index)
-      puts 'please try again!'
+  def play
+    turn until over?
+    if won?
+      puts "Congratulations player #{winner}, you won!"
+    else
+      puts 'Its a draw, play again!'
     end
   end
 end
 
-def game_full?(board)
-  board.all? { |current| current == 'x' || 'o' }
-end
-
-def game_tied?
-  !won? && board_full?
-end
-
-def valid_move?(move_index)
-  if (board[move_index] == ('x' || 'o')) || (move_index > board.length)
-    false
-  else
-    true
-  end
-end
-# rubocop:enable Metrics/CyclomaticComplexity
-# rubocop:enable Metrics/PerceivedComplexity
+ticytoe = TicTacToe.new
+ticytoe.play
