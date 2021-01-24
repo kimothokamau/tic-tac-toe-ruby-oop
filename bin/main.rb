0532a8e76873f3c097c115388e2c5c96c4997e24
kimothokamau <kimothokamau@gmail.com>
2021-01-24 00:19:29 -0600

    Added color, updated board to have numbers instead of empty strings

--- bin/main.rb
+++ bin/main.rb
@@ -1,23 +1,24 @@
 #!/usr/bin/env ruby
-require_relative '../lib/logic.rb'
+require_relative '../lib/game_logic.rb'
+require 'colorize'
 
 def intro
-  puts 'Welcome to the Tic-Tac-Toe Game!'
+  puts 'Welcome to the Tic-Tac-Toe Game!'.green
   display_board
 end
 
 def turn
   player = current_player
-  puts "Player #{player}, please enter a number between 0 and 8"
+  puts "Player #{player}, please enter a number between 0 and 8".yellow
   user_input = gets.chomp
   index = input_to_index(user_input)
   if valid_move?(index)
     move(index, player)
     @board[index] = player
     display_board
-    puts "Hey player #{player}, you selected #{index} and its turn #{turn_count}"
+    puts "Hey player #{player}, you selected #{index} and its turn #{turn_count}".green
   else
-    puts 'Please try again and enter a valid number'
+    puts 'Please try again and enter a valid number'.red
   end
 end
 
@@ -27,9 +28,9 @@ def play
   intro
   turn until over?
   if won?
-    puts "Congratulations player #{winner}, you won!"
+    puts "Congratulations player #{winner}, you won!".yellow
   else
-    puts 'Its a draw, play again!'
+    puts 'Its a draw, play again!'.red
   end
 end
 
