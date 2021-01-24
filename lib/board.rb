8b72af2015813956947ff67849eaa6beab7ebca1
kimothokamau <kimothokamau@gmail.com>
2021-01-24 17:41:54 -0600

    Remove displayboard from gamelogic

---
+++ lib/board.rb
@@ -0,0 +1,5 @@
+class Board
+  def initialize
+    @board = Array.new(9, ' ')
+  end
+end
--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -10,16 +10,6 @@ class TicTacToe
     [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
   ].freeze
 
-  def display_board
-    puts ' '
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts ' ---------- '
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts ' ---------- '
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-    puts ' '
-  end
-
   def input_to_index(user_input)
     user_input.to_i
   end
